a00e206342f972b7c09a6bd53b4c7cce81f72503
Khánh Duy <duyntk3020@gmail.com>
2024-12-30 23:58:00 +0700

    fix buggggg

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -249,16 +249,7 @@ class Homepage extends Component {
 
     render() {
         const provinces = [
-            'An Giang', 'Bà Rịa - Vũng Tàu', 'Bắc Giang', 'Bắc Kạn', 'Bạc Liêu', 'Bắc Ninh',
-            'Bến Tre', 'Bình Định', 'Bình Dương', 'Bình Phước', 'Bình Thuận', 'Cà Mau',
-            'Cần Thơ', 'Cao Bằng', 'Đà Nẵng', 'Đắk Lắk', 'Đắk Nông', 'Điện Biên', 'Đồng Nai',
-            'Đồng Tháp', 'Gia Lai', 'Hà Giang', 'Hà Nam', 'Hà Nội', 'Hà Tĩnh', 'Hải Dương',
-            'Hải Phòng', 'Hậu Giang', 'Hòa Bình', 'Hưng Yên', 'Khánh Hòa', 'Kiên Giang',
-            'Kon Tum', 'Lai Châu', 'Lâm Đồng', 'Lạng Sơn', 'Lào Cai', 'Long An', 'Nam Định',
-            'Nghệ An', 'Ninh Bình', 'Ninh Thuận', 'Phú Thọ', 'Phú Yên', 'Quảng Bình',
-            'Quảng Nam', 'Quảng Ngãi', 'Quảng Ninh', 'Quảng Trị', 'Sóc Trăng', 'Sơn La',
-            'Tây Ninh', 'Thái Bình', 'Thái Nguyên', 'Thanh Hóa', 'Thừa Thiên Huế', 'Tiền Giang',
-            'TP Hồ Chí Minh', 'Trà Vinh', 'Tuyên Quang', 'Vĩnh Long', 'Vĩnh Phúc', 'Yên Bái'
+            '< 1km', '< 2km', '< 3km', '< 5km', '< 10km', '< 15km', '>= 15km'
         ];
 
 
@@ -422,7 +413,7 @@ class Homepage extends Component {
                                 </div>
                             </div>
                             <div className="filter-group">
-                                <h4>Amenity tags</h4>
+                                <h4>{this.props.language === languages.JA ? 'アメニティ' : 'Amenity tags'}</h4>
                                 <div className='btn-group'>
                                     <button
                                         className={this.state.selectedAmenityTags.includes(Amenity.WIFI) ? 'active' : ''}
@@ -443,7 +434,7 @@ class Homepage extends Component {
                                 </div>
                             </div>
                             <div className="filter-group">
-                                <h4>Service tags</h4>
+                                <h4>{this.props.language === languages.JA ? 'サービスタグ' : 'Serviece tags'}</h4>
                                 <div className='btn-group'>
                                     <button
                                         className={this.state.selectedServiceTags.includes(Service.TABLE_SERVICE) ? 'active' : ''}
