98f0e6e65bacf269429f95842abb4fe644ba4db9
PhanTrungDuc <phantrungduc2522005@gmail.com>
2025-01-03 16:02:41 +0700

    điều hướng admin sang trang dashboard khi mới đăng nhập

--- src/routes/System.js
+++ src/routes/System.js
@@ -20,7 +20,7 @@ class System extends Component {
                 <div className="system-container">
                     <div className="system-list">
                         <Switch>
-                            <Route path="/system/user-manage" component={UserManage} />
+                            <Route path="/system/user-manage" component={Dashboard} />
                             <Route path="/system/product-manage" component={ProductManage} />
                             <Route path="/system/register-package-group-or-account" component={RegisterPackageGroupOrAcc} />
 
